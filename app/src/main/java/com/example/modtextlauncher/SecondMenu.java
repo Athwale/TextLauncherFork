4d78092a91c1318437087cba92aedb112b5aca8e
Ondrej Mejzlik <omejzlik@redhat.com>
2025-07-11 11:58:52 +0200

    Update second menu

--- app/src/main/java/com/example/modtextlauncher/SecondMenu.java
+++ app/src/main/java/com/example/modtextlauncher/SecondMenu.java
@@ -1,6 +1,11 @@
 package com.example.modtextlauncher;
 
+import static android.content.Intent.ACTION_MAIN;
+import static android.content.Intent.CATEGORY_LAUNCHER;
+
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
@@ -8,8 +13,13 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
 
 
 public final class SecondMenu extends android.app.Activity implements
@@ -19,11 +29,46 @@ public final class SecondMenu extends android.app.Activity implements
         View.OnClickListener {
 
     private final Adapter adapter = new Adapter();
+    private final ArrayList<String> ignore_list=new ArrayList<>();
+    private String start_code = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Read activity start code.
+        BufferedReader reader;
+        try {
+            final InputStream file = getAssets().open("start_code.ptx");
+            reader = new BufferedReader(new InputStreamReader(file));
+            this.start_code = reader.readLine();
+            file.close();
+        } catch (Exception e) {
+            finish();
+        }
+
+        String code = getIntent().getStringExtra("start_code");
+        if (!Objects.equals(code, this.start_code)) {
+            finish();
+        }
+
+        // todo show empty in some way when there is nothing to show.
+        // TODO in setup also set up sleep delay and shutdown delay?
         setContentView(R.layout.activity);
+
+        try {
+            final InputStream file = getAssets().open("ignored.ptx");
+            reader = new BufferedReader(new InputStreamReader(file));
+            String line = reader.readLine();
+            while (line != null) {
+                this.ignore_list.add(line);
+                line = reader.readLine();
+            }
+            file.close();
+        } catch(Exception e) {
+            Toast.makeText(this, "Reading ignore list failed",
+                    Toast.LENGTH_LONG).show();
+        }
 
         update();
         ListView list = findViewById(R.id.list);
@@ -86,10 +131,20 @@ public final class SecondMenu extends android.app.Activity implements
     }
 
     private void update() {
+        PackageManager packageManager = getPackageManager();
+        Intent intent = new Intent(ACTION_MAIN, null);
+        intent.addCategory(CATEGORY_LAUNCHER);
+        List<ResolveInfo> availableActivities = packageManager.queryIntentActivities(intent, 0);
         ArrayList<Model> models = new ArrayList<>();
         long id = 0;
-        models.add(new Model(++id, "Settings", "test"));
+        for (ResolveInfo resolveInfo : availableActivities) {
 
+            if (this.ignore_list.contains(resolveInfo.activityInfo.packageName)) {
+                models.add(new Model(++id, resolveInfo.loadLabel(packageManager).toString(),
+                        resolveInfo.activityInfo.packageName
+                ));
+            }
+        }
         models.sort(this);
         adapter.update(models);
     }
